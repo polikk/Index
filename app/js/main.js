ed9f772c97ef75f8cfb08fdbe33f6b06afe1e95c
polikk <polikigor@ukr.net>
2020-04-27 23:08:45 +0300

    +

---
+++ app/js/main.js
@@ -0,0 +1,47 @@
+$('.main-info').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+  $('.comunity-part').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true
+  });
+  $('.press-slider').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true
+  });
+  
+$('.famuous-slider').slick({
+  centerMode: true,
+  centerPadding: '10px',
+  slidesToShow: 3,
+  //infinite: true,
+  speed: 300,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  adaptiveHeight: true,
+});
+var $status = $('.pagingInfo');
+var $slickElement = $('.famuous-slider');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+  
+  var i = (currentSlide ? currentSlide : 0) + 1;
+  $status.text(i + '/' + slick.slideCount);
+});
+      
